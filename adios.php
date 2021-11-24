59742a1fd5b042fbf6b1bb970bf40024dba8012a
Mateo <sesnake99@gmail.com>
2021-11-24 11:36:59 +0100

    Archivos

---
+++ adios.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Caso Practico 2</title>
+  </head>
+  <body>
+    <?php echo "Hasta luego desarrollador!!!" ?>
+  </body>
+</html>
